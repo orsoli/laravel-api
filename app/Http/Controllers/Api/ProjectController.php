8b3dad6c4f13357cec1cb60ff175f1a263a64e95
orsoli <fljcoli@gmail.com>
2024-11-14 16:12:42 +0100

    Added route in api.php and create projectController  in Api folder, and implemented index method

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    // Implemented index method
+    public function index(){
+        // Get all projects datas from db interacting with Project Model
+        $projects = Project::all();
+        // Return all datas projects in json format
+        return response()->json([
+            'success'=>true,
+            'response'=>$projects,
+        ]);
+    }
 }
